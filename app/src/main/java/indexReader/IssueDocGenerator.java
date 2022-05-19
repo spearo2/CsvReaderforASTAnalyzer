b2a092d31e0ec60bdc6b9a11b87c364fbbd18fa8
leechanggong <leechanggong@leechanggongui-MacBookPro.local>
2022-05-20 02:33:33 +0900

    8

--- app/src/main/java/indexReader/IssueDocGenerator.java
+++ app/src/main/java/indexReader/IssueDocGenerator.java
@@ -70,7 +70,7 @@ public class IssueDocGenerator {
             while(matcher.find()) {
                 String issueKey = matcher.group(1);
                 String [] issueKeySplit = issueKey.split("-");
-                if (keyList.get("https://github.com/"+projectName.replaceAll("~","/")).equals(issueKeySplit[0])) {
+                if (keyList.get("https://github.com/"+projectName.replaceAll("~","/")) != null && keyList.get("https://github.com/"+projectName.replaceAll("~","/")).equals(issueKeySplit[0])) {
                     if (combined.containsKey(projectName)) {
                         combined.get(projectName).add(issueKey);
                     } else {
