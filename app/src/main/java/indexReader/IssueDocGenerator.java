88ae7186305b751f1acd56f7dde724a5a96c3a93
leechanggong <leechanggong@leechanggongui-MacBookPro.local>
2022-05-19 19:59:05 +0900

    a

--- app/src/main/java/indexReader/IssueDocGenerator.java
+++ app/src/main/java/indexReader/IssueDocGenerator.java
@@ -13,11 +13,53 @@ import java.util.HashMap;
 
 public class IssueDocGenerator {
     private String path;
-    private HashMap<String, ArrayList<String>> csv = new HashMap<>();
+    private ArrayList<String> keyList = new ArrayList<>();
+    private ArrayList<ArrayList<String>> csv = new ArrayList<>();
+    private ArrayList<ArrayList<String>> combined = new ArrayList<>();
+
 
     public IssueDocGenerator (String path) {
         this.path = path;
+        readKeyList();
         csvToMap();
+        makeIssuePerProject();
+    }
+
+    public void makeIssuePerProject () {
+        for (ArrayList <String> row : csv) {
+            for (String str: row) {
+                System.out.println(str);
+            }
+        }
+    }
+
+    public void readKeyList () {
+        try {
+//            Reader in = new FileReader("/data/CGYW/ASTChangeAnalyzer/data/apacheURLList.csv");
+            Reader in = new FileReader("/Users/leechanggong/Projects/ASTChangeAnalyzer/ASTChangeAnalyzer/data/apacheURLList.csv");
+            CSVParser parser = CSVFormat.EXCEL.parse(in);
+            boolean a = false;
+
+            for (CSVRecord record : parser) {
+                //ArrayList<String> temp = new ArrayList<>();
+                int b = 0;
+                for (String content:record) {
+                    if (!a) {
+                        a = true;
+                        break;
+
+                    }
+                    if (b++ == 1) {
+                        keyList.add(content);
+                        System.out.println(content);
+                    }
+
+                }
+//                listRead.add(temp);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void csvToMap () {
@@ -27,31 +69,32 @@ public class IssueDocGenerator {
         int cnt = 0;
         for (File file : listOfFiles) {
             if (file.isFile()) {
-                System.out.println(file.getName());
+                //System.out.println(file.getName());
                 cnt++;
+
+                ArrayList<String> temp = null;
+                String key = "";
+                try {
+                        Reader in = new FileReader(path + "/" + file.getName());
+                        CSVParser parser = CSVFormat.EXCEL.parse(in);
+                        for (CSVRecord record : parser) {
+                            temp = new ArrayList<String>();
+                            temp.add(file.getName());
+                            for (String str : record) {
+                                temp.add(str);
+                            }
+                            csv.add(temp);
+                        }
+                        in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
             }
         }
-        System.out.println(cnt);
-//        ArrayList<String> temp = null;
-//        String key = "";
-//        try {
-//                Reader in = new FileReader(path);
-//                CSVParser parser = CSVFormat.EXCEL.parse(in);
-//                for (CSVRecord record : parser) {
-//                    temp = new ArrayList<String>();
-//                    for (String str : record) {
-//                        if(str.contains("~")) {
-//                            temp.add(str.replace("]]", "").trim());
-//                        } else {
-//                            key = str;
-//                        }
-//                    }
-//                    csv.put(key,temp);
-//            }
-//            in.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-    }
+        //System.out.println(cnt);
+
+    //}
 }
